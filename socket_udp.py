079f136f67b36020106b48a67bb5b3dd1fdddde2
matheus-bernardino <matheusb17@hotmail.com>
2018-10-10 10:37:03 -0300

    Insere rtt e edita exceção

--- socket_udp.py
+++ socket_udp.py
@@ -5,12 +5,13 @@ from payload import *
 import pickle
 import string
 
+
 def create_server(host, port):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         print("Socket Created")
     except socket.error as error:
-        print("Failed to create socket. Error code:" + str(error[0]) + 'Message' + error[1])
+        # print("Failed to create socket. Error code:" + str(error[0]) + 'Message' + error[1])
         sys.exit()
 
     try:
@@ -23,52 +24,59 @@ def create_server(host, port):
 
     return s
 
+
 def create_client(host, port):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         print("Socket Created")
     except socket.error as error:
-        print("Failed to create socket. Error code:" + str(error[0]) + 'Message' + error[1])
+        # print("Failed to create socket. Error code:" + str(error[0]) + 'Message' + error[1])
         sys.exit()
     return s
 
-def recv(s,port):    
-    
+
+def recv(s, port):
+
     while(1):
-        
+
         data, add = s.recvfrom(4096)
         packet = pickle.loads(data)
         send_confirm(s, packet, add)
-    
-    s.close()    
 
-def send_confirm(s, header,port):
-    
-    ack = payload("ack", header.ack,header.seqNumber, header.destiny, header.font)
+    s.close()
+
+
+def send_confirm(s, header, port):
+
+    ack = payload("ack", header.ack, header.seqNumber,
+                  header.destiny, header.font)
     orig = ack.font
     print(orig, port)
     ack = pickle.dumps(ack)
     s.sendto(ack, port)
-    
-def send(s,host_s, port, data, host_c):
-    
-    aux = time.time()
 
+
+def send(s, host_s, port, data, host_c):
     seqNumber = 0
     ack = len(data.encode()) + seqNumber
-    msg = payload(data,ack , seqNumber, host_c, host_s)
+    msg = payload(data, ack, seqNumber, host_c, host_s)
     msg = pickle.dumps(msg)
+    rtt = 0.1
 
-    try :
-        s.sendto(msg, (host_s, port))
-        print(host_s, port)
+    aux = time.time()
+    while (time.time() - aux) < 5:
+        now = time.time()
+        try:
+            s.sendto(msg, (host_s, port))
+            print(host_s, port)
+            s.settimeout(rtt)
+            data, add = s.recvfrom(4096)
+            packet = pickle.loads(data)
+            print(packet.ack)
+            break
 
-    except socket.error as error:
-        print("Error Code :" + str(error[0]) + "Message" + error[1])
-        sys.exit()
-
-    while (time.time() - aux) < 4:  
-        s.settimeout(100)  
-        data, add = s.recvfrom(4096)
-        packet = pickle.loads(data)
-        print(data)
+        except socket.error as error:
+            #print("Error Code :" + str(err) + "Message" + err)
+            # sys.exit()
+            pass
+        rtt = time.time() - now
