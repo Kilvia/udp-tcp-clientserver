8edd8266c4aaada2e1fe9ad71d498cfbd1552dd6
Kilvia <kilvialeticia@hotmail.com>
2018-10-31 19:33:03 -0300

    Delete payload.py

--- payload.py
+++
@@ -1,8 +0,0 @@
-class payload():
-    def __init__(self, data, ack, seqNumber, host_c, host_s):
-      
-        self.data = data.encode()
-        self.ack = ack
-        self.seqNumber = seqNumber
-        self.font = host_c
-        self.destiny = host_s
